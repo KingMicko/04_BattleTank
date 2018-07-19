c57ce1caabdd7de89df336d321025fa059921080
MICKS-PC\Mick <mickoconnor@live.co.uk>
2018-07-19 18:01:15 +0100

    Suggest projectile velocity

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -26,7 +26,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	void AimAt(FVector HitLocationAim);
+	void AimAt(FVector HitLocationAim, float LaunchSpeed);
 	
 private:
 	UStaticMeshComponent * Barrel = nullptr;
