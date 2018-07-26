cc5fe52945ec32d18f0793c16b90b83192e6e19e
MICKS-PC\Mick <mickoconnor@live.co.uk>
2018-07-26 21:27:39 +0100

    Physics material and friction

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -21,5 +21,5 @@ public:
 	
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank and 1g acceleration
+	float TrackMaxDrivingForce = 400000000; // Assume 40 tonne tank and 1g acceleration
 };
